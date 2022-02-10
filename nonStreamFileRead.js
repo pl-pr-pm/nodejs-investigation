ab9d2e78a785b793ec696d8162f5514ef950a366
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-10 17:15:14 +0900

    Streamサンプル

--- nonStreamFileRead.js
+++ nonStreamFileRead.js
@@ -1,12 +1,9 @@
 const fs = require("fs");
 
-const copyFile = (src, dest, cb) => {
-  fs.readFile(src, (err, data) => {
-    if (err) {
-      return cb(err);
-    }
-    fs.writeFile(dest, data, cb);
-  });
+const copyFileWithStream = (src, dest, cb) => {
+  fs.createReadStream(src).pipe(fs.createWriteStream(dest)).on("finish", cb);
 };
 
-copyFile("./data/src.txt", "./data/dest.txt", (err) => console.log(err));
+copyFileWithStream("./data/src.txt", "./data/dest.txt", () =>
+  console.log("コピー完了")
+);
