535062c91c0be5b468a37bced25e1a726bac7f2f
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-13 16:39:16 +0900

    Transform Streamの値を結合し、returnできた

--- expressSSE.js
+++ expressSSE.js
@@ -4,23 +4,23 @@ const { HelloReadableStream, HelloTransformStream } = require("./HelloStream");
 
 const app = express();
 
-app.get("/api/node", (request, response) => {
+app.get("/api/node", async (request, response) => {
   response.set({
     // "Content-Type": "text/event-stream",
     "Content-Type": "application/json",
   });
-  const send = (id, data) => {
-    response.write(`id: 2\ndata: ` + data + `\n\n`);
-  };
 
   let resArray = [];
   let buf = new HelloReadableStream().pipe(new HelloTransformStream());
   count = 0;
-  buf.on("data", (chunk) => {
-    count += 1;
-    send(count, chunk);
+  for await (const line of buf) {
+    resArray.push(line);
+
+    console.log(line);
     console.log(resArray);
-  });
+  }
+
+  response.send(resArray);
 
   request.on("close", () => {
     response.end();
